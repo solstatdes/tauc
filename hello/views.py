03c71db051a3ac738dd1cae0249a6ed8c0faca09
treharne <R.Treharne@liverpool.ac.uk>
2016-08-15 20:29:54 +0100

    parse_data | basic return

--- hello/views.py
+++ hello/views.py
@@ -1,26 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import json
 
 from .models import Greeting
 from .forms import FileForm
 
-# Create your views here.
+def parse_file(fPath):
+    '''
+    This is the main data parser. Data to be checked for type and format. Error trapping for alphanumeric and missing data.
+    '''
+
+    data = []
+    for line in fPath.readlines():
+        data.append([float(x.strip("\n")) for x in line.split(",")])
+    return data
+
 def index(request):
 
-    file_upload = []
-    string = ''
+    data=[]
     if request.method == 'POST':
         file_form = FileForm(request.POST, request.FILES)
         if file_form.is_valid():
-            print('valid')
-            for line in request.FILES["newFile"].readlines():
-                print(line)
+            data = parse_file(request.FILES["newFile"])
         else:
             print file_form.errors
     else:
         file_form = FileForm()
 
-    return render(request, 'index.html', {'form': file_form, 'data': string})
+
+    return render(request, 'index.html', {'form': file_form, 'data': json.dumps(data)})
 
 
 def db(request):
